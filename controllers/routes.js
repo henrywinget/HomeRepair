30d996100cee7d4068202c3981b7f8e34f5533a6
Henry Winget <henrywinget@yahoo.com>
2018-06-08 16:37:54 -0500

    more handlebars

--- controllers/routes.js
+++ controllers/routes.js
@@ -7,22 +7,51 @@ var router = express.Router();
 
 // Route handler for displaying default homepage
 router.get("/", (req, res) => {
-    console.log(`Home page requested. ${req.body.params}`);
+    console.log(`Home page requested.`);
     res.render("index");
 });
 
 //Route handler for about-us page
 router.get("/about-us", (req, res) => {
-    console.log(`About Us page requested. ${req.body.params}`);
+    console.log(`About Us page requested.`);
     res.render("about");
 });
 
 //Route handler for contact page
 router.get("/contact-carl", (req, res) => {
-    console.log(`About Us page requested. ${req.body.params}`);
+    console.log(`Contact page requested.`);
     res.render("contact");
 });
 
+//Route handler for add a review page
+router.get("/add-review", (req, res) => {
+    console.log(`Contact page requested.`);
+    res.render("addReview");
+});
+
+//Route handler for reviews page
+router.get("/reviews", (req, res) => {
+    console.log(`Contact page requested.`);
+    res.render("review");
+});
+
+//Route handler for interior projects page
+router.get("/interior-services", (req, res) => {
+    console.log(`Contact page requested.`);
+    res.render("interior");
+});
+
+//Route handler for exterior projects page
+router.get("/exterior-services", (req, res) => {
+    console.log(`Contact page requested.`);
+    res.render("exterior");
+});
+
+//Route handler for special projects page
+router.get("/special-projects", (req, res) => {
+    console.log(`Contact page requested.`);
+    res.render("special");
+});
 
 // Export router functionality for server to use
 module.exports = router;
